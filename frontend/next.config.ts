6bca3238fef0f9b8484be08966822335e29a769a
EllinorS <ellinor.st@gmail.com>
2026-06-05 13:27:31 +0200

    fix eval()

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              `script-src 'self' 'unsafe-inline'${process.env.NODE_ENV === 'development' ? " 'unsafe-eval'" : ""}`,
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https://res.cloudinary.com",
